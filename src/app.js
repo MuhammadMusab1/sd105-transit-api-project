ec8710a5f89f4b3da53456d1710eca91658641cf
Muhammad Musab <yaqoobmusab03@gmail.com>
2021-12-12 16:20:48 -0600

    add if statement when the streetArray is empty

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const form = document.querySelector('form');
+const streetList = document.querySelector('.streets')
+streetList.innerHTML = '';
 
 
 //https://api.winnipegtransit.com/v3/streets.json?api-key=J5UJHy5yUpory_fpvpUv&name=portage&usage=long
@@ -7,7 +9,16 @@ function handleSubmit(e) {
   e.preventDefault()
   const input = document.querySelector('input');
   if (input.value !== '') {
-    getStreets(input.value)
+    getStreets(input.value).then(streetArray => {
+      if (streetArray.length === 0) {
+        const errorDIV = document.createElement('DIV');
+        errorDIV.innerHTML = 'no results were found';
+        streetList.appendChild(errorDIV);
+        return;
+      } else {
+        console.log(streetArray)
+      }
+    })
     input.value = '';
   } else {
     return;
@@ -17,7 +28,7 @@ function handleSubmit(e) {
 function getStreets(streetName) {
   return fetch(`https://api.winnipegtransit.com/v3/streets.json?api-key=J5UJHy5yUpory_fpvpUv&name=${streetName}&usage=long`)
   .then(response => response.json())
-  .then(data => console.log(data.streets))
+  .then(data => data.streets)
 }
 
 
