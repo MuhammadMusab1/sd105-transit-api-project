5feb47075870162f522f4166120cb15da8289cdd
Muhammad Musab <yaqoobmusab03@gmail.com>
2021-12-12 16:13:19 -0600

    add getStreets function

--- src/app.js
+++ src/app.js
@@ -7,12 +7,17 @@ function handleSubmit(e) {
   e.preventDefault()
   const input = document.querySelector('input');
   if (input.value !== '') {
-    console.log(input.value)
+    getStreets(input.value)
     input.value = '';
   } else {
     return;
   }
+}
 
+function getStreets(streetName) {
+  return fetch(`https://api.winnipegtransit.com/v3/streets.json?api-key=J5UJHy5yUpory_fpvpUv&name=${streetName}&usage=long`)
+  .then(response => response.json())
+  .then(data => console.log(data.streets))
 }
 
 
